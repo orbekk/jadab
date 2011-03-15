1e141387557ef341571d48311d912fcd032e3e21
Kjetil Ørbekk <kjetil.orbekk@gmail.com>
2011-03-15 08:25:06 +0000

    Implement property events.
    Properties send a PropertyChangeEvent when their set() is invoked.
    
    Include simple listener test.

--- DataBinding/src/no/ntnu/capgeminitest/data/Property.java
+++ DataBinding/src/no/ntnu/capgeminitest/data/Property.java
@@ -1,7 +1,13 @@
 package no.ntnu.capgeminitest.data;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import no.ntnu.capgeminitest.event.PropertyChangeListener;
+
 public class Property<T> {
 	T data;
+	List<PropertyChangeListener<T>> listeners = new ArrayList<PropertyChangeListener<T>>();
 	
 	public Property(T data) {
 		this.data = data;
@@ -13,5 +19,20 @@ public class Property<T> {
 	
 	public void set(T data) {
 		this.data = data;
+		notifyListeners();
+	}
+	
+	protected void notifyListeners() {
+		for (PropertyChangeListener<T> listener : listeners) {
+			listener.propertyChanged(this);
+		}
+	}
+	
+	public void addListener(PropertyChangeListener<T> listener) {
+		listeners.add(listener);
+	}
+	
+	public void removeListener(PropertyChangeListener<T> listener) {
+		listeners.remove(listener);
 	}
 }
--- DataBinding/test/no/ntnu/capgeminitest/data/PropertyTest.java
+++ DataBinding/test/no/ntnu/capgeminitest/data/PropertyTest.java
@@ -3,14 +3,39 @@ package no.ntnu.capgeminitest.data;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
+import no.ntnu.capgeminitest.event.PropertyChangeListener;
+
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class PropertyTest {
+	
+	class FakeListener implements PropertyChangeListener<Integer> {
+		Integer received = null;
+		
+		@Override
+		public void propertyChanged(Property<Integer> property) {
+			received = property.get();
+		}
+	}
+	
+	private FakeListener listener = new FakeListener();
+	Property<Integer> property;
+	
+	@Before public void setUp() {
+		property= new Property<Integer>(5);
+	}
+	
 	@Test public void testSetGet() {
-		Property<Integer> property= new Property<Integer>(5);
 		assertThat(property.get(), equalTo(5));
 		property.set(10);
 		assertThat(property.get(), equalTo(10));
+	}
+	
+	@Test public void testListener() {
+		property.addListener(listener);
+		property.set(7);
+		assertThat(listener.received, equalTo(7));
 	}
 }
