bb3575860d88717c0124464790cbbede5e912034
Kjetil Ørbekk <kjetil.orbekk@gmail.com>
2011-03-21 10:00:59 +0000

    Add dependency for updating properties.
    Properties can now listen to other properties and update themselves.

--- DataBinding/src/no/ntnu/capgeminitest/data/Property.java
+++ DataBinding/src/no/ntnu/capgeminitest/data/Property.java
@@ -17,9 +17,22 @@ import no.ntnu.capgeminitest.event.PropertyChangeListener;
  */
 public class Property<T> {
     
+    /**
+     * A listener that is used to update this property.
+     * 
+     * When this property is bound to another property, it will listen to
+     * changes to the other property and update itself on change.
+     */
+    private class OnPropertyChangeUpdater implements PropertyChangeListener<T> {
+        @Override public void propertyChanged(Property<T> property) {
+            set(property.get());
+        }      
+    }
+    
     private T data;
     private List<PropertyChangeListener<T>> listeners = new ArrayList<PropertyChangeListener<T>>();
-
+    private OnPropertyChangeUpdater updateListener = new OnPropertyChangeUpdater();
+    
     public Property(T data) {
         this.data = data;
     }
@@ -31,6 +44,17 @@ public class Property<T> {
         return data;
     }
 
+    /**
+     * Override this method to receive property changes.
+     * 
+     * Called before other bindings are notified.
+     * 
+     * Note: Changing the property inside {@code onChange} triggers a recursive call.
+     */
+    protected void onChange(T newValue) {
+        // Empty.
+    }
+    
     /**
      * Set the data for this property.
      * 
@@ -55,14 +79,7 @@ public class Property<T> {
         listeners.remove(listener);
     }
     
-    /**
-     * Override this method to receive property changes.
-     * 
-     * Called before other bindings are notified.
-     * 
-     * Note: Changing the property inside {@code onChange} triggers a recursive call.
-     */
-    protected void onChange(T newValue) {
-        // Empty.
+    PropertyChangeListener<T> getOnPropertyChangeUpdater() {
+        return updateListener;
     }
 }
