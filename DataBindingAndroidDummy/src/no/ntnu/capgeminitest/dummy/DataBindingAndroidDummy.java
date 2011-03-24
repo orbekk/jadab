b50566c374649d1f5cf06c1b0bbe5ed69ee8be70
Kjetil Ørbekk <kjetil.orbekk@gmail.com>
2011-03-24 22:48:28 +0000

    Add dummy project to make DataBindingAndroidRobolectrictest run.
    Robolectric needs to run in an Android project. This is a dummy project
    used to run the tests written for the data binding framework.

---
+++ DataBindingAndroidDummy/src/no/ntnu/capgeminitest/dummy/DataBindingAndroidDummy.java
@@ -0,0 +1,13 @@
+package no.ntnu.capgeminitest.dummy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DataBindingAndroidDummy extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
