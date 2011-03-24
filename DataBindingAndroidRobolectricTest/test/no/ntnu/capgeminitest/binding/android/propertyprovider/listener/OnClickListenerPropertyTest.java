61234a520c117809d13e03bd44eeac59ed703075
Kjetil Ørbekk <kjetil.orbekk@gmail.com>
2011-03-24 22:48:50 +0000

    Add unit test for OnClickListenerProperty.

--- DataBindingAndroidRobolectricTest/test/no/ntnu/capgeminitest/binding/android/propertyprovider/listener/OnClickListenerPropertyTest.java
+++ DataBindingAndroidRobolectricTest/test/no/ntnu/capgeminitest/binding/android/propertyprovider/listener/OnClickListenerPropertyTest.java
@@ -1,6 +1,29 @@
 package no.ntnu.capgeminitest.binding.android.propertyprovider.listener;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
+import no.ntnu.capgeminitest.data.Property;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.view.View;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+
+@RunWith(RobolectricTestRunner.class)
 public class OnClickListenerPropertyTest {
-
+    OnClickListenerProperty onClickListenerProperty = new OnClickListenerProperty();
+    
+    @Test
+    public void testOnClickListenerProperty() {
+        assertThat(onClickListenerProperty.property.get(), is(nullValue()));
+        onClickListenerProperty.onClick(null);
+        assertThat(onClickListenerProperty.property.get(), is(notNullValue()));
+        Object object1 = onClickListenerProperty.property.get();
+        onClickListenerProperty.onClick(null);
+        assertThat(onClickListenerProperty.property.get(), not(equalTo(object1)));
+    }
 }
