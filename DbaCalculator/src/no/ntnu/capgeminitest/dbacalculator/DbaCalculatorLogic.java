892dd7a26d5ece4d980a28c9d12bae744348ff48
Kjetil Ørbekk <kjetil.orbekk@gmail.com>
2011-03-25 23:12:58 +0100

    Replace tabs with spaces.

--- DbaCalculator/src/no/ntnu/capgeminitest/dbacalculator/DbaCalculatorLogic.java
+++ DbaCalculator/src/no/ntnu/capgeminitest/dbacalculator/DbaCalculatorLogic.java
@@ -19,24 +19,24 @@ package no.ntnu.capgeminitest.dbacalculator;
 import no.ntnu.capgeminitest.binding.Property;
 
 public class DbaCalculatorLogic {
-	public Property<String> text;
-	
-	private Property<Integer> number = new Property<Integer>(0) {
-		@Override protected void onChange(Integer newValue) {
-			text.set(""+newValue);
-		}
-	};
+    public Property<String> text;
+    
+    private Property<Integer> number = new Property<Integer>(0) {
+        @Override protected void onChange(Integer newValue) {
+            text.set(""+newValue);
+        }
+    };
     
     public Property<Object> numberIncrementer = new Property<Object>(null) {
         @Override protected void onChange(Object unused) {
-        	number.set(number.get() + 1);
+            number.set(number.get() + 1);
         }
     };
     
     public Property<Object> numberDecrementer = new Property<Object>(null) {
-    	@Override protected void onChange(Object unused) {
-    		number.set(number.get() - 1);
-    	}
+        @Override protected void onChange(Object unused) {
+            number.set(number.get() - 1);
+        }
     };
     
     public DbaCalculatorLogic() {
